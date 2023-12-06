af5a60d3babfe823c3039ca66f9523f5901b484d
MiliyaSokun <153126394+MiliyaSokun@users.noreply.github.com>
2023-12-06 15:52:47 -0500

    Add files via upload

--- tokenizing.c
+++ tokenizing.c
@@ -2,8 +2,9 @@
 #define _CRT_SECURE NO WARNINGS
 #define BUFFER_SIZE 300 //Define buffer size for input words
 #include "tokenizing.h"
-// VI
+//VI
 void tokenizing(void) { // use function
+/* 
     // Display message for input
     printf("*** Start of Tokenizing Words Demo ***\n");
     // Initialize variable
@@ -30,6 +31,27 @@ void tokenizing(void) { // use function
     } while (strcmp(words, "q") != 0); // continue until 'q' is entered
     // Display end message
     printf("*** End of Tokenizing Words Deno ***\n\n");
-    
+    */
+// V2 
+    printf("*** Start of tokenining Phrases Demo ***\n");
+    char    phrases[BUFFER_SIZE];
+    char*   nextPhrase = NULL;
+    int     phrasesCounter;
+    do {
+        printf("Type a few phrase separated by comma(q - to quit):\n");
+        fgets(phrases, BUFFER_SIZE, stdin);
+        phrases[strlen(phrases) - 1] = '\0';
+        if ((strcmp(phrases, "q") != 0)){
+            nextPhrase = strtok(phrases, ",");
+            phrasesCounter = 1;
+            while (nextPhrase) {
+                printf("Phrase #%d is \'%s'\n", phrasesCounter++, nextPhrase);
+                nextPhrase = strtok(NULL, ",");
+
+
+            }
+        }
+    } while (strcmp(phrases, "q") != 0);
+    printf("*** End of Tokeninzing Phrases Demo ***\n\n");
 
 }
